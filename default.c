f0f05cc9520057d362a8f84c2c87d95e09fc31a1
kgeok <{ID}+{username}@users.noreply.github.com>
2021-04-05 10:04:22 -0400

    (Commit on 'main')

---
+++ default.c
@@ -0,0 +1,26 @@
+#include "Keyboard.h"
+#include "Mouse.h"
+
+void setup()
+int responseDelay = 10;
+
+Mouse.begin();
+Keyboard.begin();
+
+{
+
+Serial.begin(9600);
+Serial.println("Starting...");
+Serial.println("");
+Serial.println("");
+
+
+}
+
+void loop()
+{
+
+
+
+
+}
